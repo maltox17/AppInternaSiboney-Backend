532c06fe7b27712099432a944d25ddeb71ce6866
Marco <marcoantonio1708@gmail.com>
2024-10-10 21:30:54 +0200

    proyecto creado

---
+++ src/main/java/com/proyecto/appInternaSiboney/AppInternaSiboneyApplication.java
@@ -0,0 +1,13 @@
+package com.proyecto.appInternaSiboney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppInternaSiboneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppInternaSiboneyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/proyecto/appInternaSiboney/AppInternaSiboneyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto.appInternaSiboney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppInternaSiboneyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
